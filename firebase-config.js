6b0e3dd5b47230e108a18d7d2bf0d6f448c1298c
Lordt121 <brixwilson26@gmail.com>
2026-06-16 22:20:04 +0100

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -6,16 +6,16 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-aut
 // Your Firebase project's unique config
 const firebaseConfig = {
   apiKey: "AIzaSyAFLuyYmbWm2mF6VGcOe7-9g8z6XA33TzE",
-    authDomain: "mcq-grading-89a51.firebaseapp.com",
-      projectId: "mcq-grading-89a51",
-        storageBucket: "mcq-grading-89a51.firebasestorage.app",
-          messagingSenderId: "571991617187",
-            appId: "1:571991617187:web:bd08c0ee80e6a492085916"
-            };
+  authDomain: "mcq-grading-89a51.firebaseapp.com",
+  projectId: "mcq-grading-89a51",
+  storageBucket: "mcq-grading-89a51.firebasestorage.app",
+  messagingSenderId: "571991617187",
+  appId: "1:571991617187:web:bd08c0ee80e6a492085916"
+};
 
-            // Boot up Firebase
-            const app = initializeApp(firebaseConfig);
+// Boot up Firebase
+const app = initializeApp(firebaseConfig);
 
-            // Export the database and auth so other files can use them
-            export const db = getFirestore(app);
-            export const auth = getAuth(app);
+// Export the database and auth so other files can use them
+export const db = getFirestore(app);
+export const auth = getAuth(app);
